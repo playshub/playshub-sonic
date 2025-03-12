27e16ea44df78ce11ccb60d4692316e6ae4ddc9a
cuonghx <cuonghx@gmail.com>
2025-03-12 11:25:54 +0700

    Remove logo when mount

--- hub-nextjs/src/components/Root/Root.tsx
+++ hub-nextjs/src/components/Root/Root.tsx
@@ -12,18 +12,9 @@ function RootInner({ children }: PropsWithChildren) {
 }
 
 export function Root(props: PropsWithChildren) {
-  // Unfortunately, Telegram Mini Apps does not allow us to use all features of
-  // the Server Side Rendering. That's why we are showing loader on the server
-  // side.
-  const didMount = useDidMount();
-
-  return didMount ? (
+  return (
     <ErrorBoundary fallback={ErrorPage}>
       <RootInner {...props} />
     </ErrorBoundary>
-  ) : (
-    <div className="root__loading">
-      <div style={{ marginTop: "100%" }}>Loading...</div>
-    </div>
   );
 }

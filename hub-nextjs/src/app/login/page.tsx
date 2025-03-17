0386719ee73455f3ece683b693282d41227e3175
cuonghx <cuonghx@gmail.com>
2025-03-17 14:20:07 +0700

    Login with Tiktok

--- hub-nextjs/src/app/login/page.tsx
+++ hub-nextjs/src/app/login/page.tsx
@@ -2,9 +2,10 @@
 import { login } from "@/apis/account/login";
 import { TELEGRAM_BOT_USERNAME } from "@/utils/constants";
 import { setUser } from "@/utils/storage";
+import Icon from "@ant-design/icons";
 import { useMutation } from "@tanstack/react-query";
 import { LoginButton, TelegramAuthData } from "@telegram-auth/react";
-import { Flex, Result } from "antd";
+import { Button, Flex, notification, Result } from "antd";
 import { useRouter } from "next/navigation";
 
 function Login() {
@@ -36,7 +37,11 @@ function Login() {
         backgroundRepeat: "no-repeat",
       }}
     >
-      <div style={{ marginTop: "auto", padding: 20, marginBottom: 80 }}>
+      <Flex
+        vertical
+        gap={10}
+        style={{ marginTop: "auto", padding: 20, marginBottom: 80 }}
+      >
         <LoginButton
           botUsername={TELEGRAM_BOT_USERNAME}
           onAuthCallback={async (data) => {
@@ -46,7 +51,19 @@ function Login() {
             router.push("/");
           }}
         />
-      </div>
+        <Button
+          type="primary"
+          icon={<Icon />}
+          style={{ fontSize: 14, fontWeight: 500, padding: "20px" }}
+          onClick={() => {
+            notification.info({
+              message: "This feature is coming soon",
+            });
+          }}
+        >
+          Login with Tiktok
+        </Button>
+      </Flex>
     </Flex>
   );
 }

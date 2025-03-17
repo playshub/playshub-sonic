cf4f8dc37b8bb149bc973a904ac3359e8fa78e01
cuonghx <cuonghx@gmail.com>
2025-03-17 13:25:56 +0700

    Fix auth

--- hub-nextjs/src/apis/account/login.ts
+++ hub-nextjs/src/apis/account/login.ts
@@ -3,7 +3,9 @@ import { setJwtToken } from "../../utils/storage";
 import api from "../axios";
 
 export const login = async (data: TelegramAuthData) => {
-  const response = await api.post("/account/login", { validate_data: data });
+  const response = await api.post("/account/login_with_telegram", {
+    validate_data: data,
+  });
   const token = response.data.access_token;
   setJwtToken(token);
   return token;
